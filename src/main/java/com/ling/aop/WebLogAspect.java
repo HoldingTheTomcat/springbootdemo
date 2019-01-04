fcbfee258cba3c990ea970ba6ef1915625aadc8c
lingzi <17778029272@163.com>
2019-01-04 15:40:08 +0800

    模拟冲突master

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -185,7 +185,7 @@ public class WebLogAspect {
         return declaringTypeName;
     }
     //测试分支合并
-    // 模拟冲突testMerge
+    // 模拟冲突master模拟冲突testMerge
     //测试提交2
     //模拟冲突master
     //测试提交33
