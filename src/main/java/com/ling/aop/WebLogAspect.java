398416da57a4aab406824e37d01128c6015b044b
lingzi <17778029272@163.com>
2019-01-14 01:42:22 +0800

    master:优先修改

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -176,7 +176,7 @@ public class WebLogAspect {
         Parameter[] parameters = method.getParameters();
         for (Parameter parameter : parameters) {
             String parameterName = parameter.getName();
-            System.out.println("优先修改");
+            System.out.println("master:优先修改");
             
         }
         Object[] args = point.getArgs();
