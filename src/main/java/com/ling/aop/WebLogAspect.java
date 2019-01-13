d992b730e53dab06b56b4be7e973bba29b94a9e1
lingzi <17778029272@163.com>
2019-01-14 01:41:33 +0800

    testMerge:我也改

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -178,6 +178,7 @@ public class WebLogAspect {
             String parameterName = parameter.getName();
             System.out.println("master:push1");
             
+            System.out.println("testMerge:我也改");
         }
         Object[] args = point.getArgs();
         if (args.length == parameters.length) {
