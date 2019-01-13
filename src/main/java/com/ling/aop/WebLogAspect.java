066c09a5c9a1dc9ccb03121779ba7a2bcb5d3ac6
lingzi <17778029272@163.com>
2019-01-14 00:07:41 +0800

    不一样的地方142

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -178,6 +178,7 @@ public class WebLogAspect {
             String parameterName = parameter.getName();
             System.out.println("不一样的地方13");
             System.out.println("不一样的地方141");
+            System.out.println("不一样的地方142");
             
         }
         Object[] args = point.getArgs();
