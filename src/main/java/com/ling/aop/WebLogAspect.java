4524361381bfae00ed493f1239ff8e15a66692fe
lingzi <17778029272@163.com>
2019-01-16 01:02:59 +0800

    全testMerge：push1

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -178,7 +178,7 @@ public class WebLogAspect {
             String parameterName = parameter.getName();
             System.out.println("我是分割线-----------------");
             System.out.println("全：push1");
-          
+            System.out.println("全testMerge：push1");
             
         }
         Object[] args = point.getArgs();
