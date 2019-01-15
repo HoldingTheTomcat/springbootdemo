50848d34601cd69d054a8a3c8671e5df46cab42f
lingzi <17778029272@163.com>
2019-01-16 01:02:59 +0800

    全testMerge：push1

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -183,6 +183,7 @@ public class WebLogAspect {
             System.out.println("全testMerge：push3");
             System.out.println("全testMerge：push4");
             System.out.println("全testMerge：push5");
+            System.out.println("全testMerge：push1");
             
         }
         Object[] args = point.getArgs();
