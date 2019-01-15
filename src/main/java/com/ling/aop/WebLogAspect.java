423dc948ad52385920824d50a3c99ae0e34b8b8e
lingzi <17778029272@163.com>
2019-01-16 01:23:24 +0800

    我是分割线-----------------

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -176,7 +176,7 @@ public class WebLogAspect {
         Parameter[] parameters = method.getParameters();
         for (Parameter parameter : parameters) {
             String parameterName = parameter.getName();
-            System.out.println("我是分割线-----------------");
+            System.out.println("我是分割线------------------");
             
         }
         Object[] args = point.getArgs();
