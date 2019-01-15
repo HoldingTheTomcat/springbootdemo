75cee1d6a81fba6f1e692ede3b7df970e1b8b9ae
lingzi <17778029272@163.com>
2019-01-15 23:11:22 +0800

    测试2

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -177,7 +177,7 @@ public class WebLogAspect {
         for (Parameter parameter : parameters) {
             String parameterName = parameter.getName();
             System.out.println("master:push1");
-            System.out.println("测试1");
+            System.out.println("测试2");
             
         }
         Object[] args = point.getArgs();
