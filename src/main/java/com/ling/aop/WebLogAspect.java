9cd94c2c49b03bb8aa56cf18e603dc0936f0712b
lingzi <17778029272@163.com>
2019-01-16 15:56:30 +0800

    testMerge:提：3-5:testMerge

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -179,6 +179,7 @@ public class WebLogAspect {
             System.out.println("我是分割线------------------");
             System.out.println("testMerge:提：1-5:testMerge");
             System.out.println("testMerge:提：2-5:testMerge");
+            System.out.println("testMerge:提：3-5:testMerge");
         }
         Object[] args = point.getArgs();
         if (args.length == parameters.length) {
