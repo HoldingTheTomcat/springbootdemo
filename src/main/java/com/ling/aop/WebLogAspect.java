e282319142eb1d1edeed937a7db7d91a01a388fa
lingzi <17778029272@163.com>
2019-01-16 14:19:38 +0800

    testMerge:时光穿梭机：1

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -177,9 +177,7 @@ public class WebLogAspect {
         for (Parameter parameter : parameters) {
             String parameterName = parameter.getName();
             System.out.println("我是分割线------------------");
-            System.out.println("testMerge:踢1");
-            System.out.println("master:踢1");
-            System.out.println("testMerge:踢2");
+            System.out.println("testMerge:时光穿梭机：1");
         }
         Object[] args = point.getArgs();
         if (args.length == parameters.length) {
