219227f6ec9632f0c9ebf0a7de8ab81afc375e53
lingzi <17778029272@163.com>
2019-01-16 17:26:12 +0800

    testMerge:3

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -180,6 +180,7 @@ public class WebLogAspect {
             System.out.println("master:1");
             System.out.println("testMerge:1");
             System.out.println("testMerge:2");
+            System.out.println("testMerge:3");
         }
         Object[] args = point.getArgs();
         if (args.length == parameters.length) {
