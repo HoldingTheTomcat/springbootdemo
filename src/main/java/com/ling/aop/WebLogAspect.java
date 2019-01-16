b5284d0dccec640712074da74b241b57d1c2fa8f
lingzi <17778029272@163.com>
2019-01-16 15:06:25 +0800

    master:提：1-3

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -177,7 +177,7 @@ public class WebLogAspect {
         for (Parameter parameter : parameters) {
             String parameterName = parameter.getName();
             System.out.println("我是分割线------------------");
-            System.out.println("testMerge:提：1");
+            System.out.println("master:提：1-3");
         }
         Object[] args = point.getArgs();
         if (args.length == parameters.length) {
