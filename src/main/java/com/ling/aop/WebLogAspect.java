8c61330503953474253a2d979cccfa8999773ec2
lingzi <17778029272@163.com>
2019-01-17 09:30:11 +0800

    master:1

--- src/main/java/com/ling/aop/WebLogAspect.java
+++ src/main/java/com/ling/aop/WebLogAspect.java
@@ -177,6 +177,7 @@ public class WebLogAspect {
         for (Parameter parameter : parameters) {
             String parameterName = parameter.getName();
             System.out.println("----------我是分割线------------------");
+            System.out.println("master:1");
         }
         Object[] args = point.getArgs();
         if (args.length == parameters.length) {
