f161d13fbff2d773c7b2d158371a9fcfece58f2e
lingzi <17778029272@163.com>
2019-02-09 10:03:29 +0800

    消息转换器-格式化

---
+++ src/main/java/com/ling/config/HttpMessageConvertConfig.java
@@ -0,0 +1,30 @@
+package com.ling.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author TianHeLing
+ * @Description
+ * @date 2019/2/6
+ */
+@Configuration
+public class HttpMessageConvertConfig {
+    
+    @Bean
+    public HttpMessageConverters fastJsonMessageConvert(){
+        //创建fastJson消息转换器
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        //创建fastJson配置对象
+        FastJsonConfig config = new FastJsonConfig();
+        //对json数据进行格式化
+        config.setSerializerFeatures(SerializerFeature.PrettyFormat, SerializerFeature.WriteDateUseDateFormat);
+        converter.setFastJsonConfig(config);
+        return new HttpMessageConverters(converter);
+    }
+    
+}
