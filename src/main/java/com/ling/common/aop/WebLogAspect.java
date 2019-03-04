afd0eb597ec99b2eec980feca6b887a96dbe345d
lingzi <17778029272@163.com>
2019-03-04 17:30:18 +0800

    日志修改，增加运行时间

---
+++ src/main/java/com/ling/common/aop/WebLogAspect.java
@@ -0,0 +1,217 @@
+package com.ling.common.aop;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.ling.common.util.IpUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author TianHeLing
+ * @Description
+ * @date 2018/12/1
+ */
+
+@Aspect
+@Component
+public class WebLogAspect {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    ThreadLocal<Long> startTime = new ThreadLocal();
+    ThreadLocal<String> uuid = new ThreadLocal();
+    
+    //定义一个切点，格式就是按切点对应的匹配格式
+    @Pointcut("execution(* com..controller..*.*(..))")
+    public void controllerPoint() {
+    }//controllerPoint就是切点名，名字任意 
+
+    @Pointcut("execution(* com..service.serviceImpl..*.*(..))")
+    public void servicePoint() {
+    }
+
+    @Pointcut("execution(* com..controller..*.*(..))||execution(* com..service.serviceImpl..*.*(..))")
+    public void commonPoint(){
+        
+    }
+
+    /**
+     * 注解类型前置通知
+     *
+     * @param point
+     */
+    @Before("commonPoint()")  //指定了切点
+    public void sayHello(JoinPoint point) {
+        this.startTime.set(System.currentTimeMillis());
+        Boolean aBoolean = this.validateIsController(point);
+        loggerPrint(point, aBoolean);
+    }
+
+
+    //后置通知，方法执行成功之后，会执行此方法，如果方法执行出现异常，那么就不会执行此方法，returning 是切点方法的返回值
+    @AfterReturning(value = "controllerPoint()", returning = "resultObject")
+    public void controllerAfterReturn(JoinPoint joinPoint, Object resultObject) throws Throwable {
+        String resultString = "";
+        if (resultObject != null) {
+            resultString = JSON.toJSONString(resultObject);
+        }
+        logger.info("controller调用结束:{} ->结果：{}", uuid.get(), resultString);
+    }
+
+    //后置通知，方法执行成功之后，会执行此方法，如果方法执行出现异常，那么就不会执行此方法，returning 是切点方法的返回值
+    @AfterReturning(value = "servicePoint()", returning = "resultObject")
+    public void serviceAfterReturn(JoinPoint joinPoint, Object resultObject) throws Throwable {
+        String resultString = "";
+        if (resultObject != null) {
+            resultString = JSON.toJSONString(resultObject);
+        }
+        logger.info("service调用结束:{} ->结果：{}", uuid, resultString);
+    }
+
+
+    //最终通知，方法执行成功或者异常，都会执行此方法
+    @After("commonPoint()")
+    public void sayGoodbeyAfter(JoinPoint point) {
+        long endTime = System.currentTimeMillis();
+        logger.info("请求执行总时间：{}",(endTime-startTime.get()));
+    }
+    
+    private void loggerPrint(JoinPoint point, boolean isController) {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+
+        String ip = IpUtils.getClientIP(request);
+        String className = getRealClassName(point);
+        // String method = request.getMethod();
+        String methodName = getMethodName(point);
+        Object[] args = point.getArgs();
+
+        //从切面织入点处通过反射机制获取织入点处的方法（被拦截的方法）
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        // String uuid = loggerMethodInfo2(ip, className, methodName, args, signature);
+        String[] parameterNames = signature.getParameterNames();
+        Class[] parameterTypes = signature.getParameterTypes();
+
+        StringBuilder paramName = new StringBuilder();
+        Class<?> returnType = signature.getReturnType();
+        String returnTypeString = returnType.toString();
+        String returnTypeName = returnTypeString.substring(returnTypeString.lastIndexOf('.') + 1, returnTypeString.length());
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        if (isController) {
+            logger.info("controller调用开始 ->{}", uuid);
+            logger.info("请求地址：{}", request.getRequestURI());
+        } else {
+            logger.info("service调用开始 ->{}", uuid);
+        }
+
+        logger.info("ip:{}", ip);
+        logger.info("调用类：{}", className);
+        logger.info("返回值类型：{}", returnType);
+        for (int i = 0; i < parameterNames.length; i++) {
+            Class parameterTypeClass = parameterTypes[i];
+            String typeClassString = parameterTypeClass.toString();
+            String parameterType = typeClassString.substring(typeClassString.lastIndexOf('.') + 1, typeClassString.length());
+            paramName.append(parameterType + " " + parameterNames[i]);
+            if (i < parameterNames.length - 1) {
+                paramName.append(",");
+            }
+        }
+        logger.info("方法：{} {}({})", returnTypeName, methodName, paramName.toString());
+        paramType2(args, parameterNames);
+        this.uuid.set(uuid);
+    }
+
+
+    private void paramType1(Object[] args, String[] parameterNames) {
+        Map<String, Object> parameterMap = new HashMap<>();
+        for (int i = 0; i < parameterNames.length; i++) {
+            if (args[i] != null) {
+                parameterMap.put(parameterNames[i], args[i]);
+            } else {
+                parameterMap.put(parameterNames[i], "null");
+            }
+        }
+        logger.info("参数: {}", JSON.toJSONString(parameterMap, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue));
+    }
+
+    private void paramType2(Object[] args, String[] parameterNames) {
+        for (int i = 0; i < parameterNames.length; i++) {
+            logger.info("参数: {}:{}", parameterNames[i], JSON.toJSONString(args[i], SerializerFeature.WriteMapNullValue));
+        }
+    }
+
+    private void paramType3(Object[] args, String[] parameterNames) {
+        StringBuilder param = new StringBuilder();
+        for (int i = 0; i < parameterNames.length; i++) {
+            param.append(parameterNames[i] + ":" + JSON.toJSONString(args[i], SerializerFeature.WriteMapNullValue));
+            if (i < parameterNames.length - 1) {
+                param.append(",");
+            }
+        }
+        logger.info("参数: {}", param.toString());
+    }
+
+    //根据类上是否有@RestController、@Controller判断是否是controller
+    private Boolean validateIsController(JoinPoint joinPoint){
+        Boolean isController = false;
+        //获取目标对象的类
+        Class<?> aClass = joinPoint.getTarget().getClass();
+        RestController restController = aClass.getAnnotation(RestController.class);
+        if (restController != null){
+            isController = true;
+        }
+        Controller controller = aClass.getAnnotation(Controller.class);
+        if (controller != null) {
+            isController = true;
+        }
+        return isController;
+    }
+
+
+    /**
+     * 异常通知,切点方法抛出异常时执行这里
+     */
+
+    @AfterThrowing(value = "controllerPoint()", throwing = "ex")
+    public void throwingMethod(Throwable ex) {
+        logger.info(ex.getMessage());
+    }
+
+    /**
+     * 获取被代理对象的真实类全名
+     *
+     * @param point 连接点对象
+     * @return 类全名
+     */
+
+    private String getRealClassName(JoinPoint point) {
+        return point.getTarget().getClass().getName();
+    }
+
+    /**
+     * 获取代理执行的方法名
+     *
+     * @param point 连接点对象
+     * @return 调用方法名
+     */
+
+    private String getMethodName(JoinPoint point) {
+        return point.getSignature().getName();
+    }
+
+}
--- src/main/java/com/ling/controller/SpringController.java
+++ src/main/java/com/ling/controller/SpringController.java
@@ -1,18 +1,25 @@
 package com.ling.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.ling.dao.entity.Student;
 import com.ling.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LingZi on 2018/11/21.
@@ -27,7 +34,7 @@ public class SpringController {
     private StudentService studentService;
 
     @GetMapping("addStudent")
-    public Student addStudent()throws Exception {
+    public Student addStudent() throws Exception {
         Student student = new Student();
         student.setDogName("lisi");
         student.setAge(10);
@@ -35,9 +42,10 @@ public class SpringController {
         return student;
     }
 
+
     @RequestMapping("validStudent")
     public String validStudent(@Valid Student student) {
-        logger.info("name: {}" ,student.getDogName());
+        logger.info("name: {}", student.getDogName());
         String aa = "";
         return "/jsp/editSelect";
     }
@@ -47,6 +55,66 @@ public class SpringController {
         List<Student> studentList = studentService.getStudentList();
         return studentList;
     }
-    
-  
+
+    @RequestMapping("gackJson")
+    public String gackJson() throws IOException {
+        ObjectMapper jackSonMapper = new ObjectMapper();
+        Student student = new Student();
+        student.setDogName("lisi");
+        // 对象转换为字符串
+        String text = jackSonMapper.writeValueAsString(student);
+        // 字符串转成对象
+        student = jackSonMapper.readValue(text, Student.class);
+
+        // 对象转换为字节流
+        byte[] bytes = jackSonMapper.writeValueAsBytes(student);
+        // 字节流转对象
+        student = jackSonMapper.readValue(bytes, Student.class);
+
+        // 写到文件
+        jackSonMapper.writeValue(new File("friend.json"), student);
+        // 从文件中读取到对象
+        student = jackSonMapper.readValue(new File("friend.json"), Student.class);
+
+
+        //map转字符串
+        Map<String, Object> map = new HashMap<>();
+        map.put("age", 25);
+        map.put("name", "yitian");
+        map.put("interests", new String[]{"pc games", "music"});
+
+        String textMap = jackSonMapper.writeValueAsString(map);
+
+        //字符串转map，从JSON转换为Map对象的时候，由于Java的类型擦除，所以类型需要我们手动用new TypeReference<T>给出
+        Map<String, Object> newMap = jackSonMapper.readValue(textMap, new TypeReference<Map<String, Object>>() {
+        });
+
+        //字符串转node
+        JsonNode root = jackSonMapper.readTree(textMap);
+        String name = root.get("name").asText();
+        int age = root.get("age").asInt();
+
+        // 美化输出
+        jackSonMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        // 允许序列化空的POJO类（否则会抛出异常）
+        jackSonMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        // 把java.util.Date, Calendar输出为数字（时间戳）
+        jackSonMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // 按Map 的key顺序输出
+        jackSonMapper.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+
+        // 在遇到未知属性的时候不抛出异常：原因: 如果json字符串中出现java对象中没有的属性，则在将json转换为java对象时会报错
+        jackSonMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+       
+        // 强制JSON 空字符串("")转换为null对象值:
+        jackSonMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+
+        // 将内容包裹为一个JSON属性，即每一层都用{}包裹，字符串转对象时会出错，即使抑制错误，转出的对象，属性也是空值，因为有些JSON文件需要这种结构。
+        jackSonMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, true);
+
+        return "/jsp/editSelect";
+    }
+
+
 }
