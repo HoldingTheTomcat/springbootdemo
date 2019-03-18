01735bd8e4d96c71aa568e9ab67fefa2b4449fc8
lingzi <17778029272@163.com>
2019-03-18 11:28:35 +0800

    channel初步使用

--- src/main/java/com/ling/manager/impl/UserServiceImpl02.java
+++ src/main/java/com/ling/manager/impl/UserServiceImpl02.java
@@ -2,12 +2,14 @@ package com.ling.manager.impl;
 
 import com.ling.manager.abstractimpl.UserManagerAbstract;
 import com.ling.manager.enums.ChannelTypeEum;
+import org.springframework.stereotype.Service;
 
 /**
  * @author TianHeLing
  * @Description
  * @date 2019/3/18
  */
+@Service
 public class UserServiceImpl02 extends UserManagerAbstract {
     @Override
     protected Integer getChannelId() {
