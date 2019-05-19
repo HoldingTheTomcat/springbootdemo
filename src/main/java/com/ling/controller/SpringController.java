2549b2168d9c1e29a1ccf2051e584f6cf2da17f4
lingzi <17778029272@163.com>
2019-05-19 17:40:25 +0800

    druid

--- src/main/java/com/ling/controller/SpringController.java
+++ src/main/java/com/ling/controller/SpringController.java
@@ -6,15 +6,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.ling.dao.entity.Student;
+import com.ling.dao.entity.StudentNew;
+import com.ling.dao.mapper.StudentNewMapper;
 import com.ling.manager.facade.UserManagerFacade;
 import com.ling.service.StudentService;
-import com.ling.util.RedisCache2;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +33,31 @@ public class SpringController {
 
     private Logger logger2 = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    private StudentNewMapper studentNewMapper;
+
+
+    /**
+     * 注意：启动项目，如果没加 dataSource.init(); 那么启动项目不进行数据库操作的情况下，不会增加连接
+     * 写个测试循环耗尽数据库链接，注意在Controller层，不要在同一个事务
+     * 再次执行 show full processlist; 查看连接是不是 增加到了 InitialSize 个链接（排除别人也在连的情况）
+     */
+    @GetMapping("getStudentNewList")
+    public List<StudentNew> getStudentNewList(String studentid, String name) {
+
+        List<StudentNew> studentList = null;
+        for (int i = 0; i < 10000; i++) {
+            studentList = studentNewMapper.selectAll();
+            try {
+                Thread.sleep(100l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return studentList;
+    }
+
     @GetMapping("getStudentList")
     @ApiOperation("查询所有学生") //描述一个类的一个方法，或者说一个接口
     @ApiImplicitParams({
@@ -41,7 +65,7 @@ public class SpringController {
             @ApiImplicitParam(name = "name",value = "学生姓名",dataType = "String",required = true)
     })
     public List<Student> getStudentList(String studentid,String name ) {
-        RedisCache2 redisCache2 = new RedisCache2();
+        // RedisCache2 redisCache2 = new RedisCache2();
         // RedisTemplate redisTemplate = redisCache2.getRedisTemplate();
         List<Student> studentList = studentService.getStudentList();
         return studentList;
