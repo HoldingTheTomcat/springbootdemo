b71fc06c52cd3de5a593b4134041a30e5da529de
HoldingTheTomcat <17778029272@163.com>
2020-01-31 12:47:19 +0800

    测试添加Rollback，回滚数据

--- src/test/java/com/ling/BaseTest.java
+++ src/test/java/com/ling/BaseTest.java
@@ -2,8 +2,10 @@ package com.ling;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by LingZi on 2018/11/21.
@@ -12,6 +14,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @SpringBootTest(classes = SpringbootdemoApplication.class) //server没有在默认位置，那么必须使用classes注明server的位置
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
+@Transactional
+@Rollback //因为是测试，所以我们不应该真的把数据插入、修改到数据库，应该回滚掉
 public class BaseTest {
     
 }
