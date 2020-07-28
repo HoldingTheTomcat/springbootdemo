543c65e9bbd53cdeb831629aad6262204b554aa4
HoldingTheTomcat <34589333+HoldingTheTomcat@users.noreply.github.com>
2020-07-28 13:21:27 +0800

    Update TestMain.java
    mast提交，测试merge

--- src/test/java/com/ling/TestMain.java
+++ src/test/java/com/ling/TestMain.java
@@ -14,7 +14,7 @@ public class TestMain {
     public static void main(String[] args) {
         List<String> letters = Arrays.asList("A", "B", "C", "D");
         // String collect = letters.stream().collect(Collectors.joining()); //ABCD
-        String collect = letters.stream().collect(Collectors.joining(",")); //A,B,C,D
+        String collect = letters.stream().collect(Collectors.joining(",")); //A,B,C,D,,,
         System.out.println(collect);
 
         String join = String.join(",", letters); //A,B,C,D
